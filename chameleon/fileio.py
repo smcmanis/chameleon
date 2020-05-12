7079a0fcb0c7b6ec10003d577ae987cd47852441
Simon McManis <simon.mcmanis@gmail.com>
2020-05-12 22:44:12 +1000

    set fileio datafiles function to return just the filename instead of relative path

--- chameleon/fileio.py
+++ chameleon/fileio.py
@@ -7,4 +7,4 @@ from typing import List
 def datafiles(directory: str) -> List[str]:
     """ all pkl files within a directory."""
     glob_pattern = os.path.join(directory, "*.pkl")
-    return glob(glob_pattern, recursive=False)
+    return [os.path.basename(x) for x in glob(glob_pattern, recursive=False)]
